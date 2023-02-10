1e49c8f8aad34acd9866508589fad97575fcd790
Jonathan Fregoso <jonathanfregoso@Jonathans-MBP.lan>
2023-02-10 13:11:41 -0800

    adding starter code

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,14 +3,9 @@ import React from 'react';
 export default function About() {
   return (
     <div>
-      <h1>About Page</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+      <h1>About Me</h1>
+      <p class ='card'>
+        Hello welcome to my profile, my name is Jonathan Fregoso i am 35 years old i live in the city of LA, I am looking to achieve my goal to attain a new skill set in coding/development. 
       </p>
     </div>
   );
